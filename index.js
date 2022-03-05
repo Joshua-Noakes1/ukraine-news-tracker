a5d7cee99e1d5dcff7e8785b041d2675285775d5
Joshua Noakes <joshua.noakes1@icloud.com>
2022-03-05 17:08:53 +0000

    add working scraper

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-const browserObject = require('./lib/browser');
-const scraperController = require('./lib/pageController');
+const lcl = require('cli-color'),
+    path = require('path'),
+    browserObject = require('./lib/browser/browser'),
+    scraperController = require('./lib/browser/pageController'),
+    asw = require('./lib/asw'),
+    CronJob = require('cron').CronJob,
+    {
+        readJSON: read
+    } = require('./lib/readwrite');
+
 
 async function main() {
-    //Start the browser and create a browser instance
+    // Start the browser and create a browser instance
     let browserInstance = await browserObject.startBrowser();
 
-    // Pass the browser instance to the scraper controller
-    await scraperController(browserInstance, 'https://www.bbc.co.uk/news/live/world-europe-60532634/'); // bbc news
-    await scraperController(browserInstance, 'https://ukraine.liveuamap.com/'); // uamap
+    // load in list of sites
+    let sites = await read(path.join(__dirname, 'data', 'sites.json'), true);
 
+    // kill if cant load sites
+    if (sites.success !== true) {
+        console.log(lcl.red("[FS - Error]"), "Failed to load sites.json");
+        return process.exit(1);
+    }
+
+    // loop through sites and take screenshots
+    await asw(sites.sites, async (site, index, array) => {
+        console.log(lcl.blue("[Cron - Info]"), `Navigating to site ${index + 1}/${array.length} - ${site.name}`);
+        await scraperController(browserInstance, site.url); // goto site
+    });
+
+    // close browser
     await browserInstance.close();
 }
 
 main();
+
+// setup cron for every 30 minutes
+// let job = new CronJob('0 */30 * * * *', async function () {
+//     console.log(lcl.blue("[Cron - Info]"), "Cron has passed, taking screenshots");
+//     await main(); // start the main function
+// }, null, true, process.env.TZ);
+// job.start();
